706403a02d39e988af6b6a8add77e8c749f17074
watssk90-ctrl <watssk90@maneenininvest.com>
2026-06-02 10:31:36 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ const APP_CONFIG = {
   APP_SUBTITLE: "อำเภอพิมาย จังหวัดนครราชสีมา",
   VERSION: "2.0.0",
 
-  https://script.google.com/macros/s/AKfycbzH3c9whKGkwlQMKVhTD-5H8cOkpyW0rTrVuL-hOAyZX9YdwfHX2Y-Xprh6wnnQN4tm/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzH3c9whKGkwlQMKVhTD-5H8cOkpyW0rTrVuL-hOAyZX9YdwfHX2Y-Xprh6wnnQN4tm/exec",
 
   SESSION_KEY: "samegla_session",
   TOKEN_KEY: "samegla_token",
